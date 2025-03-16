794143388a898f7b998d676c73c6e583ed69742f
Dheeraj <dheerajbudigi@gmail.com>
2025-03-16 18:39:05 +0530

    asd

--- src/navbar.jsx
+++ src/navbar.jsx
@@ -6,11 +6,26 @@ const Navbar = () => {
 
   return (
     <nav className="nav">
+      {/* Home Button */}
       <button className="nav-home" onClick={() => navigate('/')} aria-label="Go to Home">
         Community Connect
       </button>
+
+      {/* Search Bar */}
       <input type="text" className="nav-search" placeholder="Search Discussions" aria-label="Search" />
-      <button className="nav-profile" onClick={() => navigate('/Profile')} aria-label="Go to Profile Page">
+
+      
+
+      {/* Login & Signup Buttons */}
+      <button className="nav-login" onClick={() => navigate('/login')} aria-label="Login"> 
+        Login
+      </button>
+      <button className="nav-signup" onClick={() => navigate('/signup')} aria-label="Sign Up"> 
+        Sign Up
+      </button>
+
+      {/* Profile Button */}
+      <button className="nav-profile" onClick={() => navigate('/profile')} aria-label="Go to Profile Page">
         Profile
       </button>
     </nav>
