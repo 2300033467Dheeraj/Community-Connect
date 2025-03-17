079d8d99474f6789ebd8009268817b90be6c66b5
Dheeraj <dheerajbudigi@gmail.com>
2025-03-17 12:30:27 +0530

    add

--- src/main.jsx
+++ src/main.jsx
@@ -7,6 +7,8 @@ import Profile from './profile';
 import Create from './Create';
 import Threads from './Profiledetails/Threads';
 import Replies from './Profiledetails/Replies';
+import Login from './Login';
+import Signup from './Signup';
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
     <Router>
@@ -17,6 +19,8 @@ ReactDOM.createRoot(document.getElementById('root')).render(
           <Route path="Create" element={<Create />} />
           <Route path="Profile/Threads" element={<Threads />} />
           <Route path="Profile/Replies" element={<Replies />} />
+          <Route path="Login" element={<Login />} />
+          <Route path="Signup" element={<Signup />} />
         </Route>
       </Routes>
     </Router>
